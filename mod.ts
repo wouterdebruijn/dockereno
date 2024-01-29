fbf2376b0bee6f5c9afb480a38e52ca832c4a771
Wouter de Bruijn <wouter@hedium.nl>
2024-01-29 13:01:32 +0100

    ✨ Added exports of classes

--- mod.ts
+++ mod.ts
@@ -2,24 +2,4 @@ import DockerClient from "./docker/DockerClient.ts";
 import DockerRuntime from "./docker/entities/DockerRuntime.ts";
 import UnixSocketClient from "./socket/UnixSocketConnection.ts";
 
-const dockerClient = new DockerClient(
-  new UnixSocketClient("/var/run/docker.sock"),
-);
-
-const dockerRuntime = new DockerRuntime(dockerClient);
-
-// Create alpine container sending hello world to stdout every 5 seconds
-// const container = await dockerRuntime.createContainer({
-//   image: "alpine",
-//   cmd: ["sh", "-c", "while true; do echo hello world; sleep 5; done"],
-//   tty: true,
-// });
-
-// // Start the container
-// await container.start();
-
-const containers = await dockerRuntime.getContainers();
-
-for await (const message of containers[0].attach()) {
-  console.log(message)
-}
+export { DockerClient, DockerRuntime, UnixSocketClient };
